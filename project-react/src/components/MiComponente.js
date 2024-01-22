f0f0e5ebf0edc58bf9af23c961533847ac2359ac
jhreyes483 <jhreyes483@misena.edu.co>
2024-01-22 16:35:03 -0500

    recorrer datos de un array

--- project-react/src/components/MiComponente.js
+++ project-react/src/components/MiComponente.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-class MiComponente extends React.Component{
+class MiComponente extends Component{
 
     render(){ // renderiza
+
+        let receta = {
+            nombre:       'Pizza',
+            ingredientes: ['Tomate','Queso', 'Jamon cocido'],
+            calorias:     400
+        }
+
         return (
-            <h1>Hola, soy el componente llamado: MiComponente</h1>
+            <div className='mi-componentte'> 
+                <h1>{'Receta '+receta.nombre}</h1>
+                <h2>{'Calorias '+receta.calorias}</h2>
+
+                <ol>               
+                    {
+                    receta.ingredientes.map((ingrediente, i) =>{
+                        return(
+                            <li key={i}>
+                                {ingrediente}
+                            </li>
+                        )
+                    })
+                    }
+                </ol> 
+                <hr/>
+            </div>
+
         );
     }
 }
